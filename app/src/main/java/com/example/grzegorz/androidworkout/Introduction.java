378328ea7b0303b2594cce67cfb72aa6d00fa0f0
Gboz <gbolesta21@gmail.com>
2016-11-20 17:36:09 +0100

    ListView to another Activity -> implementation

---
+++ app/src/main/java/com/example/grzegorz/androidworkout/Introduction.java
@@ -0,0 +1,18 @@
+package com.example.grzegorz.androidworkout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Grzegorz on 20.11.2016.
+ */
+
+public class Introduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+
+    }
+}
--- app/src/main/java/com/example/grzegorz/androidworkout/SecondActivity.java
+++ app/src/main/java/com/example/grzegorz/androidworkout/SecondActivity.java
@@ -1,21 +1,54 @@
 package com.example.grzegorz.androidworkout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    ListView listView;
+    String[] lessons = {
+            "Wstęp",
+            "Diagram przypadków użycia",
+            "Diagram klas",
+            "Diagram czynności",
+            "Diagram maszyny stanowej",
+            "Diagram interakcji",
+            "Diagram sekwencji",
+            "Diagram komunikacji",
+            "Diagram harmonogrowania",
+            "Diagram sterowania interakcją",
+            "Diagramy wdrożeniowe",
+            "Diagram pakietów"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        String[] values = new String[] {"Wstęp", "Diagram przypadków użycia", "Diagram klas", "Diagram czynności", "Diagram maszyny stanowej", "Diagram interakcji", "Diagram sekwencji", "Diagram komunikacji", "Diagram harmonogrowania", "Diagram sterowania interakcją", "Diagramy wdrożeniowe", "Diagram pakietów"};
-        ListView listView = (ListView) findViewById(R.id.list_of_lessons);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        listView = (ListView) findViewById(R.id.list_of_lessons);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lessons);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent introductionIntent = new Intent(SecondActivity.this, Introduction.class);
+                        startActivity(introductionIntent);
+                        break;
+                    case 1:
+                        Intent dpuIntent = new Intent(SecondActivity.this, MainActivity.class);
+                        startActivity(dpuIntent);
+                    default:
+                        break;
+                }
+            }
+        });
+
     }
+
 }

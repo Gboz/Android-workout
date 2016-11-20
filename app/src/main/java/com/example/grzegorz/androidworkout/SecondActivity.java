584cb69029d3bcd3873c1699f06efaffaabcc068
Gboz <gbolesta21@gmail.com>
2016-11-20 12:52:25 +0100

    fix Main Activity and add ListView to SecondActivity

--- app/src/main/java/com/example/grzegorz/androidworkout/SecondActivity.java
+++ app/src/main/java/com/example/grzegorz/androidworkout/SecondActivity.java
@@ -2,6 +2,8 @@ package com.example.grzegorz.androidworkout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        String[] values = new String[] {"Wstęp", "Lekcja 1", "Lekcja 2", "Lekcja 3"};
+        ListView listView = (ListView) findViewById(R.id.list_of_lessons);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        listView.setAdapter(adapter);
     }
 }

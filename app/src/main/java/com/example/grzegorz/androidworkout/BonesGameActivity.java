278a336acc9eadb130c30ec9844e4da333d4a85f
Gboz <gbolesta21@gmail.com>
2017-01-06 18:48:20 +0100

    Add method: randomThrowRange which randomize int var  from range and return convert this int to String

--- app/src/main/java/com/example/grzegorz/androidworkout/BonesGameActivity.java
+++ app/src/main/java/com/example/grzegorz/androidworkout/BonesGameActivity.java
@@ -22,32 +22,26 @@ public class BonesGameActivity extends AppCompatActivity {
         cB4 = (CheckBox) findViewById(R.id.cB4);
         cB5 = (CheckBox) findViewById(R.id.cB5);
         playerAction = (Button) findViewById(R.id.playerButton);
+        playerAction.setText("Wyrzuć");
 
         playerAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cB1.setText("1");
-                cB2.setText("2");
-                cB3.setText("3");
-                cB4.setText("4");
-                cB5.setText("5");
+
+                cB1.setText(randomThrowRange(1, 6));
+                cB2.setText(randomThrowRange(1, 6));
+                cB3.setText(randomThrowRange(1, 6));
+                cB4.setText(randomThrowRange(1, 6));
+                cB5.setText(randomThrowRange(1, 6));
             }
         });
-
-        cB1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cB1.setText("Test");
-                if (cB1.getText() == "Test") {
-                    cB1.setText("Dupa");
-                } else {
-                    cB1.setText("Oko");
-                }
-            }
-        });
-
     }
 
-
+    public String randomThrowRange(int min, int max) {
+        int range = (max - min) + 1;
+        int result = ((int)(Math.random() * range) + min);
+        String s = Integer.toString(result);
+        return s;
+    }
 
 }
